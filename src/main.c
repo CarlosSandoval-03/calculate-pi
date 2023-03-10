c6685e479f778f1ea728eddf51c00212911282b8
Carlos Sandoval <csandovalc@unal.edu.co>
2023-03-10 18:36:35 -0500

    chore: edit console messages

--- src/main.c
+++ src/main.c
@@ -4,15 +4,15 @@
 
 int main(int argc, char **argv)
 {
-	printf("START ONE PROCESS\n");
+	printf("START ONE PROCESS...\n");
 	double execution_time = run_one_process();
 	printf("\nExecution time: %f secs\n", execution_time);
 
-	printf("\n\nSTART TWO PROCESS\n");
+	printf("\n\nSTART TWO PROCESS...\n");
 	execution_time = run_two_process();
 	printf("\nExecution time: %f secs\n", execution_time);
 
-	printf("\n\nSTART FOUR PROCESS\n");
+	printf("\n\nSTART FOUR PROCESS...\n");
 	execution_time = run_four_process();
 	printf("\nExecution time: %f secs\n", execution_time);
 	return EXIT_SUCCESS;
